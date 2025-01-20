584e83c4e7a6d38acf14c767425c84adb5a3ebef
vivekbharadwaj009 <140109492+vivekbharadwaj009@users.noreply.github.com>
2025-01-20 18:31:14 +0530

    Create Nine.py

---
+++ Nine.py
@@ -0,0 +1,35 @@
+#9. Build a model using multi linear regression algorithm on any dataset.
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+data = pd.read_csv('50_Startups.csv')
+df = pd.DataFrame(data)
+
+print("Dataset:")
+print(df.head())
+
+X = df.drop(columns=['Profit'])
+y = df['Profit']
+
+column_transformer = ColumnTransformer(
+transformers=[('encoder', OneHotEncoder(), ['State'])], remainder='passthrough')
+X = column_transformer.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print(f"Mean Squared Error: {mse}")
+print(f"R-squared Score: {r2}")
+
+print("Coefficients:", model.coef_)
+print("Intercept:", model.intercept_)
